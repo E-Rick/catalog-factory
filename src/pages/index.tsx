631b8384910e794b88155ac95daf8248f11d4f78
Erick Martinez <erick@wrecs.studio>
2022-08-05 16:28:07 -0400

    add: ether hooks

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,44 @@
-import { FC } from 'react'
+import type { NextPage } from 'next'
 import { APP_NAME } from '@/lib/consts'
 import MintForm from '@/components/MintForm'
 import { Box, Text } from 'degen'
 import { ConnectButton } from '@rainbow-me/rainbowkit'
+import contractInterface from '@/utils/contract-abi.json'
+import { usePrepareContractWrite, useContractWrite, useContractRead, useWaitForTransaction } from 'wagmi'
 
-const Home: FC = () => {
+const contractConfig = {
+	addressOrName: '0x86fbbb1254c39602a7b067d5ae7e5c2bdfd61a30',
+	contractInterface: contractInterface,
+}
+
+const Home: NextPage = () => {
+	const { config: contractWriteConfig } = usePrepareContractWrite({
+		...contractConfig,
+		functionName: 'mint',
+	})
+
+	const {
+		data: mintData,
+		write: mint,
+		isLoading: isMintLoading,
+		isSuccess: isMintStarted,
+		error: mintError,
+	} = useContractWrite(contractWriteConfig)
+
+	// const { data: totalSupplyData } = useContractRead({
+	// 	...contractConfig,
+	// 	functionName: 'totalSupply',
+	// 	watch: true,
+	// })
+
+	const {
+		data: txData,
+		isSuccess: txSuccess,
+		error: txError,
+	} = useWaitForTransaction({
+		hash: mintData?.hash,
+	})
+
 	return (
 		<>
 			<Box display="flex" padding="6" marginBottom="40" alignItems="center" justifyContent="space-between">
