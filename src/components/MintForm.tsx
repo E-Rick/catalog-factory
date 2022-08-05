134506cd55aa7e07b82ee73884b81cb56d6c6209
Erick Martinez <erick@wrecs.studio>
2022-08-05 16:30:36 -0400

    refactor: mintform

--- src/components/MintForm.tsx
+++ src/components/MintForm.tsx
@@ -15,11 +15,16 @@ import {
 import { useForm, SubmitHandler } from 'react-hook-form'
 import React, { useState } from 'react'
 
+type Image = 'image/jpg' | 'image/jpeg' | 'image/png' | 'image/webp' | 'image/svg+xml' | 'image/tiff' | 'image/gif'
+type Audio = 'audio/wav' | 'audio/wave' | 'audio/x-wav' | 'audio/aiff'
+
 interface IFormInput {
 	name: string
 	description: string
 	image: string
 	price: number
+	coverImage: string
+	song: Audio
 	sellerFundRecipient: string
 }
 
@@ -33,6 +38,19 @@ const MintForm = () => {
 	const [result, setResult] = useState('')
 	const onSubmit: SubmitHandler<IFormInput> = data => setResult(JSON.stringify(data))
 	console.log('errors: ', errors)
+	// const onChange = e => {
+	// 	const file = e.target.files[0]
+	// 	const storageRef = app.storage().ref()
+	// 	const fileRef = storageRef.child(file.name)
+	// 	fileRef.put(file).then(() => {
+	// 		console.log('Uploaded a file')
+	// 	})
+	// }
+
+	// const onSubmitFile = data => {
+	// 	console.log(data)
+	// }
+
 	console.log('name: ', watch('name')) // watch input value by passing the name of it
 	console.log('price: ', watch('price')) // watch input value by passing the name of it
 	return (
@@ -42,57 +60,19 @@ const MintForm = () => {
 					<Stack direction="horizontal" space="4">
 						<Stack space="10">
 							<FieldSet legend="Create a song">
-								<Textarea id="bio" label="Description"></Textarea>
-								<MediaPicker
-									accept="image/jpeg, image/png, image/webp"
-									compact
-									label="Choose or drag and drop a song"
-								/>
-								<FileInput
-									onChange={file => alert(file)}
-									accept="audio/mpeg,audio/mp3,audio/vnd.wav,audio/vnd.wave,audio/wav,audio/wave,audio/x-wav,audio/aiff"
-								>
-									{context =>
-										context.name ? (
-											<Stack align="center" direction="horizontal">
-												{context.name}
-												{context.previewUrl}
-												<Button
-													shape="circle"
-													size="small"
-													variant="transparent"
-													onClick={context.reset}
-												>
-													<VisuallyHidden>Remove</VisuallyHidden>
-													<IconClose />
-												</Button>
-											</Stack>
-										) : (
-											<Box>{context.droppable ? 'Drop file' : 'Attach file'}</Box>
-										)
-									}
-								</FileInput>
-
+								<Input label="Song title" required placeholder="Keep it Heady" {...register('name')} />
+								<Textarea label="Description" {...register('description')}></Textarea>
 								<Input
-									name="name"
-									label="Song title"
-									required
-									placeholder="Keep it Heady"
-									{...register('name')}
-								/>
-
-								<Input
-									name="price"
 									label="Price"
 									step="0.000001"
-									placeholder="10"
+									placeholder="0.01"
 									suffix="ETH"
 									type="number"
 									{...register('price')}
 								/>
+								<input type="file" name="song" ref={register} {...register('song')} />
 
 								<Input
-									name="sellerFundRecipient"
 									description="The address that will receive any withdrawals and royalties. It can be your personal wallet, a multisignature wallet, or an external splits contract."
 									label="Seller fund recipient"
 									placeholder="0xA0Cf…251e"
