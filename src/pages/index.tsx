4c575f43a545ea5000e98305c8a55ffe3db7154c
Sweets Sweetman <sweetmantech@gmail.com>
2022-08-06 20:19:25 -0300

    I remove unused code.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,39 +3,11 @@ import { APP_NAME } from '@/lib/consts'
 import MintForm from '@/components/MintForm'
 import { Box, Text } from 'degen'
 import { ConnectButton } from '@rainbow-me/rainbowkit'
-import contractInterface from '@/utils/contract-abi.json'
 import getFactoryAddress from '@/utils/getFactoryAddress'
-import { usePrepareContractWrite, useContractWrite, useContractRead, useWaitForTransaction, useNetwork } from 'wagmi'
-
-const contractConfig = {
-	addressOrName: '0x86fbbb1254c39602a7b067d5ae7e5c2bdfd61a30',
-	contractInterface: contractInterface,
-}
+import { useNetwork } from 'wagmi'
 
 const Home: NextPage = () => {
-	const { config: contractWriteConfig } = usePrepareContractWrite({
-		...contractConfig,
-		functionName: 'mint',
-	})
 	const { chain } = useNetwork();
-
-	const {
-		data: mintData,
-		write: mint,
-		isLoading: isMintLoading,
-		isSuccess: isMintStarted,
-		error: mintError,
-	} = useContractWrite(contractWriteConfig)
-
-	const {
-		data: txData,
-		isSuccess: txSuccess,
-		error: txError,
-	} = useWaitForTransaction({
-		hash: mintData?.hash,
-	})
-	console.log('USE CONTRACT ADDRESS FOR CHAIN: ', getFactoryAddress(chain?.id))
-
 
 	return (
 		<>
