2469b355e15571b45b858bf4ef9a864b07dfa0a8
Sweets Sweetman <sweetmantech@gmail.com>
2022-08-06 19:20:32 -0300

    I fix typeScript build errors.

--- src/components/MintForm.js
+++ src/components/MintForm.js
@@ -1,15 +1,15 @@
-import {
-	Box,
-	Stack,
-	FieldSet,
-	Input,
-	Textarea,
-	Button,
-} from 'degen'
+import { Box, Stack, FieldSet, Input, Textarea, Button } from 'degen'
 import { useForm, SubmitHandler, FormProvider } from 'react-hook-form'
 import React, { useEffect, useState } from 'react'
 import { MediaPicker } from './MediaPicker.tsx'
-import { useAccount, useContract, useContractWrite, usePrepareContractWrite, useSigner, useWaitForTransaction } from 'wagmi'
+import {
+	useAccount,
+	useContract,
+	useContractWrite,
+	usePrepareContractWrite,
+	useSigner,
+	useWaitForTransaction,
+} from 'wagmi'
 import MintSongButton from '@/components/MintSongButton'
 import { NFTStorage } from 'nft.storage'
 import contractInterface from './MintSongButton/contract-abi.json'
@@ -20,26 +20,9 @@ const contractConfig = {
 	contractInterface: contractInterface,
 }
 
-// read the API key from an environment variable. You'll need to set this before running the example!
-const API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDU0NUY3MmE2RTE4ZTc1REZBMTA3Qjc3REIzNDM1NDNjOTQzMEI0RmQiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTY1OTc5NDQwMDc1MCwibmFtZSI6IlpPUkEifQ.9Jv78U50b9Ik3te-AJNcSvMCGPt402uwV6cbQyCzXiU"
-
-interface IFormInput {
-	// song name
-	name: string
-	description: string
-	token: string
-	image: File
-	song: File
-	// native gas token askPrice (eth/matic) wei
-	askPrice: number
-	findersFeeBps: number
-	// The address to send funds to once the NFT is sold
-	sellerFundsRecipient: string
-}
-
 const MintForm = () => {
-	const {address} = useAccount()
-	const [loading,setLoading] = useState(false)
+	const { address } = useAccount()
+	const [loading, setLoading] = useState(false)
 	const { data: signer } = useSigner()
 	const contract = useContract({
 		addressOrName: '0x83439E53bfcD6398B9b315f96a5dB689B82bfa0A',
@@ -51,11 +34,11 @@ const MintForm = () => {
 	const {
 		register,
 		handleSubmit,
-		setValue, reset,
+		setValue,
 		formState: { errors },
-	} = useForm<IFormInput>()
-	
-	const onSubmit: SubmitHandler<IFormInput> = async data => {
+	} = useForm()
+
+	const onSubmit = async data => {
 		setLoading(true)
 		const metadata = createMusicMetadata(data)
 		// Validate fields
@@ -72,40 +55,49 @@ const MintForm = () => {
 	}
 
 	const deployCatalog = async (metadata, curatorName, sellerFundsRecipient, askPrice, findersFee) => {
-
 		await contract
-			.createCatalog(
-				curatorName,
-				metadata,
-				askPrice,
-				sellerFundsRecipient || address,
-				findersFee,
-				{ value: 500000000000000 }
-			)
+			.createCatalog(curatorName, metadata, askPrice, sellerFundsRecipient || address, findersFee, {
+				value: 500000000000000,
+			})
 			.then(async tx => {
 				const receipt = await tx.wait()
 			})
 			.catch(console.error)
-		
 	}
 
-	useEffect(()=> {
-		register("image");
-		register("song")
-	},[])
+	useEffect(() => {
+		register('image')
+		register('song')
+	}, [])
 
 	return (
 		<Box display="grid" gap="8" marginX="12" margin="8">
-			<FormProvider {...methods} > 
-			<form onSubmit={handleSubmit(onSubmit)}>
+			<FormProvider {...methods}>
+				<form onSubmit={handleSubmit(onSubmit)}>
 					<Stack direction="horizontal" space="4">
 						<Stack space="10">
 							<FieldSet legend="Create a song">
-								<Input id='name' label="Song title" required placeholder="Keep it Heady" {...register('name')} />
-								<Input id='token' prefix='$' maxLength={5} label="Token" textTransform='uppercase' required placeholder="MUSIC" {...register('token')} />
-								<Textarea id='description' label="Description" {...register('description')}></Textarea>
+								<Input
+									id="name"
+									label="Song title"
+									required
+									placeholder="Keep it Heady"
+									{...register('name')}
+								/>
+								<Input
+									id="token"
+									prefix="$"
+									maxLength={5}
+									label="Token"
+									textTransform="uppercase"
+									required
+									placeholder="MUSIC"
+									{...register('token')}
+								/>
+								<Textarea id="description" label="Description" {...register('description')}></Textarea>
 								{/* todo: change out the suffix to matic when network is switched */}
-								<Input id='ask-price'
+								<Input
+									id="ask-price"
 									label="Ask Price"
 									step="0.000001"
 									placeholder="0.01"
@@ -113,9 +105,9 @@ const MintForm = () => {
 									type="number"
 									{...register('askPrice')}
 								/>
-								
+
 								<Input
-									id='finders-fee'
+									id="finders-fee"
 									label="Finder's Fee"
 									step="1"
 									placeholder="5"
@@ -125,18 +117,36 @@ const MintForm = () => {
 									{...register('findersFeeBps')}
 								/>
 								{/* todo: add media player to start music */}
-								<MediaPicker id='song' compact maxSize={20} accept='audio/wav, audio/mp3, audio/wave, audio/mpeg' label='Upload your sound' onError={e => console.log(e)} onChange={e => {
-									console.log('Audio:',e)
-									setValue('song', e)
-								}} name='song' />
-								<MediaPicker id='image' compact accept='image/jpeg, image/png, image/webp' label='Cover image' onError={e => console.log(e)} onChange={e => { 
-								console.log('Image:',e)
-									setValue('image', e)}} />
-								
-								<Input id='seller-fund-recipient'
+								<MediaPicker
+									id="song"
+									compact
+									maxSize={20}
+									accept="audio/wav, audio/mp3, audio/wave, audio/mpeg"
+									label="Upload your sound"
+									onError={e => console.log(e)}
+									onChange={e => {
+										console.log('Audio:', e)
+										setValue('song', e)
+									}}
+									name="song"
+								/>
+								<MediaPicker
+									id="image"
+									compact
+									accept="image/jpeg, image/png, image/webp"
+									label="Cover image"
+									onError={e => console.log(e)}
+									onChange={e => {
+										console.log('Image:', e)
+										setValue('image', e)
+									}}
+								/>
+
+								<Input
+									id="seller-fund-recipient"
 									description="The address that will receive any withdrawals and royalties. It can be your personal wallet, a multisignature wallet, or an external splits contract."
 									label="Seller fund recipient"
-									placeholder={address ?? "0xA0Cf…251e"}
+									placeholder={address ?? '0xA0Cf…251e'}
 									{...register('sellerFundsRecipient')}
 								/>
 							</FieldSet>
@@ -148,9 +158,8 @@ const MintForm = () => {
 							</Box>
 						</Stack>
 					</Stack>
-			</form>
+				</form>
 			</FormProvider>
-
 		</Box>
 	)
 }
