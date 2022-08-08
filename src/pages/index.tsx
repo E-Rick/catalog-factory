d65d3041eca103614b60181571f3c5ec8cf88fdc
Erick Martinez Jr <erick@erickjr.com>
2022-08-08 00:26:26 -0400

    add: mounted hook to index and remove props for hooks

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,23 @@
 import type { NextPage } from 'next'
-import { APP_NAME } from '@/lib/consts'
-import MintForm from '@/components/MintForm'
+import { APP_NAME } from '@/utils/consts'
 import { Box, Text } from 'degen'
 import { ConnectButton } from '@rainbow-me/rainbowkit'
-import getFactoryAddress from '@/utils/getFactoryAddress'
-import getZoraModuleManagerAddress from '@/utils/getZoraModuleManagerAddress'
 import { useNetwork } from 'wagmi'
+import { useIsMounted } from '@/hooks'
+import MintForm from '@/components/MintForm'
 
 const Home: NextPage = () => {
+	const isMounted = useIsMounted()
 	const { chain } = useNetwork();
-
+	if (!isMounted) return null
+	
 	return (
 		<>
 			<Box display="flex" padding="6" marginBottom="12" alignItems="center" justifyContent="space-between">
 				<Text size="large">{APP_NAME}</Text>
 				<ConnectButton chainStatus="icon" accountStatus="avatar" />
 			</Box>
-			<MintForm contractAddress={getFactoryAddress(chain?.id)} moduleManagerContractAddress={getZoraModuleManagerAddress(chain?.id)} />
+			{chain && <MintForm/>}
 		</>
 	)
 }
