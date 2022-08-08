657f53184e34778eaaa61c63040243b16f7431b9
Erick Martinez Jr <44077419+E-Rick@users.noreply.github.com>
2022-08-08 12:06:57 -0400

    Update src/hooks/useIsMounted.ts
    Co-authored-by: sweetman.eth <sweetmantech@gmail.com>

--- src/hooks/useIsMounted.ts
+++ src/hooks/useIsMounted.ts
@@ -1,7 +1,7 @@
 import {useState, useEffect} from 'react'
 
 export function useIsMounted() {
-  const [mounted, setMounted] = React.useState(false)
+  const [mounted, setMounted] = useState(false)
 
   React.useEffect(() => setMounted(true), [])
 
