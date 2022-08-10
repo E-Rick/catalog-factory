314fff3d4244008c596952baa4f290190aa564c4
Erick Martinez Jr <erickmartinezjunior@gmail.com>
2022-08-10 12:25:02 -0400

    add: progress bar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,11 +4,15 @@ import { AppProps } from 'next/app'
 import 'degen/styles'
 import '../styles/index.css'
 import '../styles/theme.scss'
+import '../styles/progress.css';
 import '@rainbow-me/rainbowkit/styles.css'
 import { chain, configureChains, createClient, WagmiConfig } from 'wagmi'
-import { ThemeProvider } from 'degen'
+import { ThemeProvider, vars } from 'degen'
 import { alchemyProvider } from 'wagmi/providers/alchemy'
 import { publicProvider } from 'wagmi/providers/public'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import NProgress from 'nprogress';
 
 const { chains, provider } = configureChains(
 	[chain.polygonMumbai, chain.polygon, chain.mainnet, chain.rinkeby],
@@ -18,13 +22,22 @@ const { chains, provider } = configureChains(
 const { connectors } = getDefaultWallets({ appName: APP_NAME, chains })
 const wagmiClient = createClient({ autoConnect: true, connectors, provider })
 
+
 const App = ({ Component, pageProps }: AppProps): JSX.Element => {
+	const router = useRouter();
+
+  useEffect(() => {
+    router.events.on('routeChangeStart', () =>  NProgress.start());
+    router.events.on('routeChangeComplete', () =>  NProgress.done());
+    router.events.on('routeChangeError', () =>  NProgress.done());
+  }, []);
+
 	return (
 		<ThemeProvider defaultMode="dark" defaultAccent="yellow">
 			<WagmiConfig client={wagmiClient}>
 				<RainbowKitProvider chains={chains} theme={darkTheme({
-					accentColor: '#FFC061',
-					accentColorForeground: 'white',
+					accentColor: vars.colors.yellow,
+					accentColorForeground: vars.colors.white,
 					borderRadius: 'small',
 					fontStack: 'system',
 					overlayBlur: 'small',
